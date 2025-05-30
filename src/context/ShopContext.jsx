96efe0b163d7c4168f889a6cbf8e5231a145c9b4
PC - 15 <amsanthoshkumar2@gmail.com>
2025-05-30 17:08:47 +0530

    Added new code and files

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 import { products } from "../assets/frontend_assets/assets";
-
+import React, { useState } from "react";
 export const ShopContext = createContext();
 
 
@@ -8,12 +8,18 @@ export const ShopContextProvider = (props) => {
     
      const currency ='₹';
      const deliveryFee = 30;
-
+ 
+      const [search, setSearch] = useState('');
+      const [showSearch, setShowSearch] = useState(true);
 
     const value = {
      products,
       currency, 
       deliveryFee,
+      search,
+      setSearch,
+      showSearch,
+      setShowSearch
     }
     return(
         <ShopContext.Provider value={value}>
