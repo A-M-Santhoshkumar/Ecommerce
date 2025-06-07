cee11044ac6080500e744b581a08ced7e488e111
Santhoshkumar <a.m.santhoshkumar02@gmail.com>
2025-06-07 19:49:13 +0530

    about and contect pages updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,17 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 
+  import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
  
   return (
      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
+       <toast/>
       <Navbar/>
       <SearchBar/>
       <Routes>
